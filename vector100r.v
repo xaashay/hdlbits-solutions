cebfaa12475f2ddf1eb045b12496a186000be1a7
aashay <32699111+xaashay@users.noreply.github.com>
2025-12-30 23:01:32 +0530

    Create vector100r.v

---
+++ vector100r.v
@@ -0,0 +1,9 @@
+// reverse contents of vector in using an always block. generate can be used as well but will require module instantiations
+module top_module (input [99:0] in, output [99:0] out )  
+  integer i;
+  always @ (in) begin
+    for (i = 0; i<100; i++)
+      out[99-i] = in[i]
+    end
+  end
+endmodule
