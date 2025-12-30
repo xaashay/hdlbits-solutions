41a0960b412e9809f299db0cccdbf876d5e19851
aashay <32699111+xaashay@users.noreply.github.com>
2025-12-30 23:06:52 +0530

    Update vector100r.v

--- vector100r.v
+++ vector100r.v
@@ -3,7 +3,7 @@ module top_module (input [99:0] in, output [99:0] out )
   integer i;
   always @ (in) begin
     for (i = 0; i<100; i++)
-      out[99-i] = in[i]
+      out[99-i] = in[i];
     end
   end
 endmodule
